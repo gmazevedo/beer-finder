c7202d8dc07212285810d9ccef3d34c2240d9884
Gessica Azevedo <gessicamendonca.a@gmail.com>
2025-11-13 18:06:20 -0300

    fills beer model

--- app/Models/BeerStore.php
+++ app/Models/BeerStore.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class BeerStore extends Pivot
 {
-    //
+    protected $fillable = [
+        'beer_id',
+        'store_id',
+        'price',
+        'url',
+        'promo_label',
+    ];
+
+    public function casts(): array
+    {
+        return [
+            'price' => 'integer',
+        ];
+    }
 }
