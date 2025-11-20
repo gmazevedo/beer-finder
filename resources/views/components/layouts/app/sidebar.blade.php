d98500e19e24979d6308339a53f3d558b43f11ec
Gessica Azevedo <gessicamendonca.a@gmail.com>
2025-11-20 00:16:31 -0300

    adds Lojas to navlist

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -15,6 +15,7 @@
                 <flux:navlist.group :heading="__('Platform')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
                     <flux:navlist.item icon="beaker" :href="route('beers.index')" :current="request()->routeIs('beers.* ')">Cervejas</flux:navlist.item>
+                    <flux:navlist.item icon="building-storefront" :href="route('stores.index')" :current="request()->routeIs('stores.* ')">Lojas</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
