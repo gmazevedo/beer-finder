0aa93ffa346b168baadd2d5e379534b6c7e623f8
Gessica Azevedo <gessicamendonca.a@gmail.com>
2025-11-20 00:16:12 -0300

    creates routes for Stores

--- routes/web.php
+++ routes/web.php
@@ -36,4 +36,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('beers', \App\Livewire\Beers\Index::class)->name('beers.index');
     Route::get('beers/create',\App\Livewire\Beers\Create::class)->name('beers.create');
     Route::get('beers/{beer}',\App\Livewire\Beers\Update::class)->name('beers.update');
+    Route::get('stores', \App\Livewire\Store\Index::class)->name('stores.index');
+    Route::get('stores/create', \App\Livewire\Store\Create::class)->name('stores.create');
+    Route::get('stores/{store}', \App\Livewire\Store\Update::class)->name('stores.update');
+
 });
