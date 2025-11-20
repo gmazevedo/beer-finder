646c311f94fef10e742ee4a700536f47950a6740
Gessica Azevedo <gessicamendonca.a@gmail.com>
2025-11-20 00:14:25 -0300

    adds missing return types

--- app/Livewire/Beers/Update.php
+++ app/Livewire/Beers/Update.php
@@ -11,19 +11,19 @@ class Update extends Component
     public BeerForm $form;
     public Beer $beer;
 
-    public function mount(Beer $beer)
+    public function mount(Beer $beer): void
     {
         $this->beer = $beer;
         $this->form->setBeer($beer);
     }
 
-    public function save()
+    public function save(): \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
     {
         $this->form->update();
         return redirect(route('beers.index'))
             ->success("{$this->form->name} atualizada com sucesso!");
     }
-    public function render()
+    public function render(): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
     {
         return view('livewire.beers.update');
     }
